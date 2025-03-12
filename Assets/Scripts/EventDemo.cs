776f631896064b4380fed8ecf91f2355c9d2796f
echo-plus-r <tostesi@shernet.sheridancollege.ca>
2025-03-12 11:29:04 -0400

    fixed up some code in the event demo script.

--- Assets/Scripts/EventDemo.cs
+++ Assets/Scripts/EventDemo.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -11,12 +12,17 @@ public class EventDemo : MonoBehaviour
     public float timelen = 2;
     public float t;
 
+    
+
     private void Update()
     {
+        // no clue what the below line does or if it's actually needed 
+        //OnTimerEnd.AddListener(MouseExit);
         t += Time.deltaTime;
         if (t > timelen) 
         {
             OnTimerEnd.Invoke();
+            Debug.Log("SOMETHING HAPPENED!");
             t = 0;
         }
         //else
