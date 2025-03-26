3d5593269bf1ab2af347a655da9bea7a4152d2c3
echo-plus-r <tostesi@shernet.sheridancollege.ca>
2025-03-26 11:13:51 -0400

    did the jumping part of the "Trick Shot Remastered" coding gym challanege. I'll leave the particle system thing for later, as honestly I have no clue what I'm looking at.

--- Assets/Scripts/walkin.cs
+++ Assets/Scripts/walkin.cs
@@ -9,6 +9,10 @@ public class walkin : MonoBehaviour
     AudioSource au;
     public float speed = 2;
     public AudioClip[] sounds;
+    public AnimationCurve curve;
+    float t = 1f;
+    Vector2 start;
+    bool jumping = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,16 +24,38 @@ public class walkin : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector2 pos = transform.position;
         float direction = Input.GetAxis("Horizontal");
         sr.flipX = (direction < 0);
         animaor.SetFloat("movement", Mathf.Abs(direction));
-        
-
+        if (Input.GetKeyDown("space") && !jumping)
+        {
+            t = 0f;
+            start.y = pos.y;
+            jumping = true;
+            animaor.SetFloat("jumping", 1);
+        }
+        if (t < 1 && jumping)
+        {
+            pos.y = start.y + curve.Evaluate(t);
+            t += 0.5f * Time.deltaTime;
+        }
+        else
+        {
+            jumping = false;
+            animaor.SetFloat("jumping", 0);
+        }
+        transform.position = pos;
         transform.position += transform.right * direction * speed * Time.deltaTime;
     }
     public void step() 
     {
         au.clip = sounds[Random.Range(0, 9)];
         au.Play();
+    }
+
+    public void land()
+    {
+
     }
 }
