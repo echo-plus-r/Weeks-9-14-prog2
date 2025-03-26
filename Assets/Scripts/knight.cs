3e76889a3fcb85b3959fb376473f9cfb02f0c14f
echo-plus-r <tostesi@shernet.sheridancollege.ca>
2025-03-26 10:02:41 -0400

    did the in class activity thing. only a little broken this time :)

---
+++ Assets/Scripts/knight.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+//using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class knight : MonoBehaviour
+{
+    Animator animaor;
+    SpriteRenderer sr;
+    public float speed = 2;
+    public bool canRun = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animaor = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float direction = Input.GetAxis("Horizontal");
+        sr.flipX = (direction < 0);
+        animaor.SetFloat("movement", Mathf.Abs(direction));
+
+        if (Input.GetMouseButton(0))
+        {
+            animaor.SetTrigger("attack");
+        
+        }
+        if (canRun == true)
+        {
+            transform.position += transform.right * direction * speed * Time.deltaTime;
+        }
+
+        
+        //transform.position += transform.right * direction * speed * Time.deltaTime;
+    }
+    public void AttackHasFinished()
+    {
+        Debug.Log("Ready to Run again");
+        canRun = true;
+    }
+}
