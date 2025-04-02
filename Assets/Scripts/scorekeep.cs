d22cd10d4b46256ec1fbc77330bfaf39f140d9d5
DESKTOP-2933QV0\simon <tostesi@sheridancollege.ca>
2025-04-02 13:40:44 -0400

    got the event from a balloon popping from being too big to work, kind of. the functionality of "hear event, change score" is there, but it seems like it can only take refrence to objects in the scene. i'll fix this, hopefully.

--- Assets/Scripts/scorekeep.cs
+++ Assets/Scripts/scorekeep.cs
@@ -6,24 +6,25 @@ using UnityEngine;
 public class scorekeep : MonoBehaviour
 {
     public balloon balloon;
-    TextMeshPro tmp;
-    int score = 0;
+    TMP_Text tmp;
+    int score = 200;
     // Start is called before the first frame update
     void Start()
     {
         balloon.tooBig.AddListener(popped);
+        tmp = GetComponent<TMP_Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        tmp.SetText(score.ToString());
     }
 
     public void popped()
     {
         Debug.Log("pong");
         score -= 100;
-        tmp.SetText(score.ToString());
+        
     }
 }
