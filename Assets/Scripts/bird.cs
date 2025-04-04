2805c502873d0b1124c054a1bd1e4d06ec6a15fb
DESKTOP-2933QV0\simon <tostesi@sheridancollege.ca>
2025-04-04 08:31:54 -0400

    made the bird teleport to the left if it's off the screen to the right. made it so the bird now listens for the balloon spawning event and the balloon popping event. bird now changes speeds depending on what event it hears. added comments to bird script.

--- Assets/Scripts/bird.cs
+++ Assets/Scripts/bird.cs
@@ -4,18 +4,59 @@ using UnityEngine;
 
 public class bird : MonoBehaviour
 {
+    // refrence to the spawning script. this is used so we can know when a balloon is spawned
+    public spawningscript spawningscript;
+
+    // pos holds the object's position
     Vector3 pos;
+
+    // speed holds how fast the bird moves
+    float speed = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // adding a listener that listens to the spawner
+        spawningscript.spawned.AddListener(MakeListener);   
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        // checks if the bird is off the right side of the screen
+        if (transform.position.x > 10)
+        {
+            // teleports the bird to off the left side of the screen
+            pos.x = -11;
+            transform.position = pos;
+        }
+
+        // getting the object's position and setting it as pos
         pos = transform.position;
-        pos.x += 1f * Time.deltaTime;
+
+        // adding to pos based on speed
+        pos.x += speed * Time.deltaTime;
+
+        // setting the object's position
         transform.position = pos;
     }
+
+    // this function slows the bird down, it's called when a balloon is popped.
+    // note that this function takes a float that it does not use, this is because the ChangeScore event passes a float.
+    // i decided to just not use the float instead of make a whole new event, as it would keep thing simple with only a single event the balloon sends out when it's popped.
+    public void speedChange(float x)
+    {
+        speed -= 0.5f;
+    }
+
+    // this is mostly copied over from the spawning scorekeep script
+    // this function runs every time a balloon is spawned
+    // it adds a new listener to itself to listen to events from the new balloon, it also speeds up the bird.
+    public void MakeListener(balloon nb)
+    {
+        speed += 0.5f;
+        nb.ChangeScore.AddListener(speedChange);
+    }
+
 }
