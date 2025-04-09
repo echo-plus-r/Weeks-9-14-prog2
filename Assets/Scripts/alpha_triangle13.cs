63f63e1af1a4417b0748ffe22b09b4f8e2b12eae
DESKTOP-2933QV0\simon <tostesi@sheridancollege.ca>
2025-04-09 11:51:10 -0400

    fixed up the code a little i think.

--- Assets/Scripts/alpha_triangle13.cs
+++ Assets/Scripts/alpha_triangle13.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class alpha_triangle13 : MonoBehaviour
 {
-    Vector2 mousepos, pos;
+    Vector2 mousepos, pos, rot;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,8 +16,12 @@ public class alpha_triangle13 : MonoBehaviour
     {
         mousepos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         pos = transform.position;
-        pos.x += (mousepos.x / 5) * Time.deltaTime;
-        pos.y += (mousepos.y / 5) * Time.deltaTime;
+        rot = mousepos - (Vector2)transform.position;
+        //pos.x += (mousepos.x / 5) * Time.deltaTime;
+        //pos.y += (mousepos.y / 5) * Time.deltaTime;
+
+        pos.y += 0.1f * Time.deltaTime;
+        transform.up = rot;
         transform.position = pos;
     }
 }
